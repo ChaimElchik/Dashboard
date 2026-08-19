cddffd96dec678ca4a42ba8c8117776ca9fee13b
ChaimElchik <54806860+ChaimElchik@users.noreply.github.com>
2026-08-19 11:25:24 +0200

    Update photos.js

--- photos.js
+++ photos.js
@@ -10,9 +10,9 @@
   The three included images are placeholders and can simply be deleted.
 */
 var DASHBOARD_PHOTOS = [
-  "20240809_122115.jpg",
-  "20240911_151947.jpg",
-  "20260511_161359.jpg",
-  "20260512_101338.jpg",
-  "878592-B119031_N00341_ID878592_FF_P001.jpg"
+  "photos/20240809_122115.jpg",
+  "photos/20240911_151947.jpg",
+  "photos/20260511_161359.jpg",
+  "photos/20260512_101338.jpg",
+  "photos/878592-B119031_N00341_ID878592_FF_P001.jpg"
 ];
