67e51dcd266390c60d22d3a5daed004699583ee7
ChaimElchik <54806860+ChaimElchik@users.noreply.github.com>
2026-08-19 11:22:17 +0200

    Update photos.js

--- photos.js
+++ photos.js
@@ -10,7 +10,9 @@
   The three included images are placeholders and can simply be deleted.
 */
 var DASHBOARD_PHOTOS = [
-  "photos/demo-1.jpg",
-  "photos/demo-2.jpg",
-  "photos/demo-3.jpg"
+  "20240809_122115.jpg",
+  "20240911_151947.jpg",
+  "20260511_161359.jpg",
+  "20260512_101338.jpg",
+  "878592-B119031_N00341_ID878592_FF_P001.jpg"
 ];
